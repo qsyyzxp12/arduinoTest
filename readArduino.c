96b5effd39b38e14133696fa749344574ed60654
Lin Chih An <qsyyzxp12@yahoo.com.tw>
2016-05-12 09:00:31 +0000

    signal handler

--- readArduino.c
+++ readArduino.c
@@ -4,19 +4,26 @@
 #include<wiringSerial.h>
 #include<errno.h>
 #include<fcntl.h>
+#include<signal.h>
 
 #define MAX_MES_SIZE 32
 
 char* errMes;
+int fd;
 
 char* readSerial(int fd);
 int handShake(int fd);
+void endSerial(int fd);
+void signalHandler();
 
 int main()
 {
+	signal(SIGINT, signalHandler);
+	signal(SIGTERM, signalHandler);
 	errMes = malloc(sizeof(char)*64);
 	bzero(errMes, sizeof(char)*64);
-	int fd = serialOpen("/dev/ttyACM0", 9600);
+
+	fd = serialOpen("/dev/ttyACM0", 9600);
 	if(fd == -1)
 	{
 		perror("serialOpen");
@@ -26,42 +33,64 @@ int main()
 	int ret = handShake(fd);
 	if(!ret)
 	{
-		printf("HandShake: %s\n", errMes);
+		printf("HandShake Error\n");
+		endSerial(fd);
 		return 1;
 	}
 	printf("HandShake success\n");
+	while(1)
+		printf("%s", readSerial(fd));
+
+	endSerial(fd);
 	return 0;
+}
+
+void signalHandler()
+{
+	printf("signal catched\n");
+	endSerial(fd);
+	exit(1);
+}
+
+void endSerial(int fd)
+{
+	char reqMes[] = "End";
+	serialPuts(fd, reqMes);
+	serialClose(fd);
 }
 
 int handShake(int fd)
 {
-	int ret;
 	char reqMes[] = "Arduino?";
-	while(!(ret = serialDataAvail(fd)))
+	int errCount = 0;
+	while(errCount < 5)
 	{
-		int i=0;
-		for(i=0; i<strlen(reqMes); i++)
-			serialPutchar(fd, reqMes[i]);
-		sleep(1);
+		int ret;
+		while(!(ret = serialDataAvail(fd)))
+		{
+			int i=0;
+			for(i=0; i<strlen(reqMes); i++)
+				serialPutchar(fd, reqMes[i]);
+			sleep(1);
+		}
+		printf("Serial Data Available!\n");
+		char* recvMes = readSerial(fd);
+		if(!strcmp(recvMes, "YES\r\n"))
+			return 1;
+		else if(!strcmp(recvMes, "NO\r\n"))
+		{
+			printf("Receive mes \"NO\"");
+			errCount++;
+			continue;
+		}
+		else
+		{
+			printf("Receive upexpected message - %s", recvMes);
+			errCount++;
+			continue;
+		}
 	}
-	printf("Serial Data Available!\n");
-	char* recvMes = readSerial(fd);
-	if(!strcmp(recvMes, "YES\r\n"))
-		return 1;
-	else if(!strcmp(recvMes, "NO\r\n"))
-	{
-		bzero(errMes, sizeof(char)*64);
-		strcat(errMes, "Receive mes \"NO\"");
-		return 0;
-	}
-	else
-	{
-		bzero(errMes, sizeof(char)*64);
-		errMes = strcat("Receive unexpected message - ", recvMes);
-		strcat(errMes, recvMes);
-		return 0;
-	}
-	
+	return 0;
 }
 
 char* readSerial(int fd)
